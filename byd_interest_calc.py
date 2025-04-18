db5dcb85868bbb3dabd32f54c9c474ab32218a57
yukasaraahh <118827912+yukasaraahh@users.noreply.github.com>
2025-04-18 12:50:33 +0700

    Update byd_interest_calc.py

--- byd_interest_calc.py
+++ byd_interest_calc.py
@@ -200,7 +200,7 @@ with col_img:
 # --------- Calculations & Results ---------
 period_options = [48, 60, 72, 84]
 st.markdown("""
-<div style='margin: 0 0 12px 0; border-top: 1px solid #ddd;'></div>
+<div style='margin: 0 0 4px 0; border-top: 1px solid #ddd;'></div>
 """, unsafe_allow_html=True)
 
 if st.session_state.show_result and input_valid and price > 0 and not down_payment_df.empty:
